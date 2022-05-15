06fb5ef946563bef05eee84a50030ba58dd2fbd5
Rai Daniyal <rai.dani.dev@gmail.com>
2022-05-15 23:51:44 +0500

    SchemaImplementation::Votes, posts and comments

---
+++ db/migrate/20180807075819_create_comments.rb
@@ -0,0 +1,14 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.text :body, null: false
+      t.belongs_to :user, null: false
+      t.belongs_to :post, null: false
+
+      t.timestamps null: false
+    end
+
+    add_foreign_key :comments, :users
+    add_foreign_key :comments, :posts
+  end
+end
